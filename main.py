df60660edb241550590379240a7d5c4d387e24b4
shiningstar37 <56527909+shiningstar37@users.noreply.github.com>
2021-11-11 19:12:25 +1100

    kalman filter

--- main.py
+++ main.py
@@ -6,54 +6,58 @@ import time
 import imageio
 
 images = []
-tracker = Tracker(50,3)
+tracker = Tracker(40,3,200)
 track_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0),
 				(127, 127, 255), (255, 0, 255), (255, 127, 255),
 				(127, 0, 255), (127, 0, 127),(127, 10, 255), (0,255, 127)]
 
 
-def task1_1(root = '../Sequences/01/'):	
-	for img_name in os.listdir(root):
-		img_path = os.path.join(root,img_name)
-		img = cv2.imread(img_path)	
-		frame = np.ones(img.shape,np.uint8)*255
+# def task1_1(root = '../Sequences/01/'):	
+# 	i = 0
+# 	for img_name in os.listdir(root):
+# 		img_path = os.path.join(root,img_name)
+# 		img = cv2.imread(img_path)	
+# 		frame = np.ones(img.shape,np.uint8)*255
 
 
-		_, img_binarized = cv2.threshold(img, 130, 255, cv2.THRESH_BINARY_INV)
-		img_gray = cv2.cvtColor(img_binarized, cv2.COLOR_BGR2GRAY)
-		img_gray = cv2.medianBlur(img_gray, 7) 
+# 		_, img_binarized = cv2.threshold(img, 130, 255, cv2.THRESH_BINARY_INV)
+# 		img_gray = cv2.cvtColor(img_binarized, cv2.COLOR_BGR2GRAY)
+# 		img_gray = cv2.medianBlur(img_gray, 7) 
 
-		contours, hierarchy = cv2.findContours(img_gray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)	
-		centers = []
-		for c in contours:
-			(x, y), radius = cv2.minEnclosingCircle(c)
-			centers.append((x,y))
+# 		_, contours, hierarchy = cv2.findContours(img_gray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)	
+# 		centers = []
+# 		for c in contours:
+# 			(x, y), radius = cv2.minEnclosingCircle(c)
+# 			centers.append((x,y))
 
-		centers = np.array(centers)
+# 		centers = np.array(centers)
 
-		if (len(centers) > 0):
-			tracker.update(centers,contours)
-			for j in tracker.cur_frame:
-				x = int(tracker.tracks[j].trace[-1][0])
-				y = int(tracker.tracks[j].trace[-1][1])
-				track_color = track_colors[tracker.tracks[j].trackId % len(track_colors)]
-				cv2.putText(frame,str(tracker.tracks[j].trackId), (x-10,y-20),0, 0.5, track_color,2)
-				cv2.drawContours(frame,tracker.tracks[j].contour, -1, track_color, 3)
+# 		if (len(centers) > 0):
+# 			tracker.update(centers,contours)
+# 			for j in tracker.cur_frame:
+# 				x = int(tracker.tracks[j].trace[-1][0])
+# 				y = int(tracker.tracks[j].trace[-1][1])
+# 				track_color = track_colors[tracker.tracks[j].trackId % len(track_colors)]
+# 				cv2.putText(frame,str(tracker.tracks[j].trackId), (x-10,y-20),0, 0.5, track_color,2)
+# 				cv2.drawContours(frame,tracker.tracks[j].contour, -1, track_color, 3)
 
 
-			cv2.imshow('image',frame)
-			# cv2.imwrite("image"+str(i)+".jpg", frame)
-			# images.append(imageio.imread("image"+str(i)+".jpg"))
-			time.sleep(0.5)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				cv2.destroyAllWindows()
-				break
+# 			cv2.imshow('image',frame)
+# 			# cv2.imwrite("image"+str(i)+".jpg", frame)
+# 			# images.append(imageio.imread("image"+str(i)+".jpg"))
+# 			i += 1
+# 			time.sleep(0.5)
+# 			if cv2.waitKey(1) & 0xFF == ord('q'):
+# 				cv2.destroyAllWindows()
+# 				break
 			
 
 
 
 
-def task1_2(root = '../Sequences/01/'):	
+def main(root = '../Sequences/01/'):	
+	initial = True
+	coords = {}
 	for img_name in os.listdir(root):
 		img_path = os.path.join(root,img_name)
 		img = cv2.imread(img_path)	
@@ -64,30 +68,63 @@ def task1_2(root = '../Sequences/01/'):
 		img_gray = cv2.cvtColor(img_binarized, cv2.COLOR_BGR2GRAY)
 		img_gray = cv2.medianBlur(img_gray, 7) 
 
-		contours, hierarchy = cv2.findContours(img_gray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)	
+		_, contours, hierarchy = cv2.findContours(img_gray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)	
 		centers = []
 		for c in contours:
 			(x, y), radius = cv2.minEnclosingCircle(c)
 			centers.append((x,y))
 
 		centers = np.array(centers)
+		print(centers)
 
 		if (len(centers) > 0):
-			tracker.update(centers,contours)
+			# 1.2
+			print(len(centers))
+			tracker.update(centers, contours)
+			# for j in range(len(tracker.tracks)):
+			# 	print(str(tracker.tracks[j].trackId) + str(tracker.tracks[j].trace))
+			distance = 0
+			active_trackers = 0
+			coords_new = {}
 			for j in range(len(tracker.tracks)):
 				if (len(tracker.tracks[j].trace) > 1):
-					x = int(tracker.tracks[j].trace[-1][0])
-					y = int(tracker.tracks[j].trace[-1][1])
+					coords_new[tracker.tracks[j].trackId] = tracker.tracks[j].trace[-1]
+					x = int(tracker.tracks[j].trace[-1][0,0])
+					y = int(tracker.tracks[j].trace[-1][0,1])
 					tl = (x-10,y-10)
 					br = (x+10,y+10)
 					track_color = track_colors[tracker.tracks[j].trackId % len(track_colors)]
 					cv2.rectangle(frame,tl,br,track_color,1)
 					cv2.putText(frame,str(tracker.tracks[j].trackId), (x-10,y-20),0, 0.5, track_color,2)
+					cv2.drawContours(frame,tracker.tracks[j].contour, -1, track_color, 3)
 					for k in range(len(tracker.tracks[j].trace)):
-						x = int(tracker.tracks[j].trace[k][0])
-						y = int(tracker.tracks[j].trace[k][1])
+						x = int(tracker.tracks[j].trace[k][0,0])
+						y = int(tracker.tracks[j].trace[k][0,1])
 						cv2.circle(frame,(x,y), 3, track_color,-1)
 					cv2.circle(frame,(x,y), 6, track_color,-1)
+
+			# 2.3
+			for j in range(len(tracker.tracks)):
+				if (len(tracker.tracks[j].trace) > 1):
+					# print(tracker.tracks[j].trace[-1])
+					# print(coords_new)
+					# print(coords)
+					try:
+						d = np.sqrt((coords_new[tracker.tracks[j].trackId][0,0]-coords[tracker.tracks[j].trackId][0,0])**2+(coords_new[tracker.tracks[j].trackId][0,1]-coords[tracker.tracks[j].trackId][0,1])**2)
+						if d > 0:
+							distance += d
+							active_trackers += 1
+							print(str(tracker.tracks[j].trackId) + ": " + str(distance) + " active trackers " + str(active_trackers))
+						# print(coords[tracker.tracks[j].trackId])
+						coords[tracker.tracks[j].trackId] = coords_new[tracker.tracks[j].trackId]
+					except KeyError:
+						coords[tracker.tracks[j].trackId] = coords_new[tracker.tracks[j].trackId]
+						
+			if active_trackers == 0:
+				print(img_name + " Need second image to calculate avg displacement")
+			else:
+				distance = distance / active_trackers
+				print(img_name + " Avg displacement: " + str(distance))
 
 			cv2.imshow('image',frame)
 			# cv2.imwrite("image"+str(i)+".jpg", frame)
@@ -100,5 +137,5 @@ def task1_2(root = '../Sequences/01/'):
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
 if __name__ == '__main__':
-	task1_1('../Sequences/01/')
-	#task1_2()
+	# task1_1()
+	main()
--- tracker.py
+++ tracker.py
@@ -1,62 +1,166 @@
 import numpy as np 
 from kalmanFilter import KalmanFilter
 from scipy.optimize import linear_sum_assignment
+from collections import deque
+
+# class Tracks(object):
+# 	def __init__(self, detection, trackId):
+# 		super(Tracks, self).__init__()
+# 		self.KF = KalmanFilter()
+# 		self.KF.predict()
+# 		self.KF.correct(np.matrix(detection).reshape(2,1))
+# 		self.trace = deque(maxlen=200)
+# 		self.prediction = detection.reshape(1,2)
+# 		self.trackId = trackId
+# 		self.skipped_frames = 0
+
+# 	def predict(self,detection,contour):
+# 		# pred = self.KF.predict()
+# 		# if len(self.trace)==0:
+# 		# 	self.prediction = detection.reshape(1,2)
+# 		# else:
+# 		# 	self.prediction = np.array(pred).reshape(1,2)
+# 		# self.KF.correct(np.matrix(detection).reshape(2,1))
+# 		# self.trace.append(detection)
+# 		self.contour = contour
+# 		self.prediction = np.array(self.KF.predict()).reshape(1,2)
+# 		self.KF.correct(np.matrix(detection).reshape(2,1))
+	
+# 	# def self_play(self):
+# 	# 	self.trace.append(self.KF.predict())
+
+
+# class Tracker(object):
+# 	def __init__(self, dist_threshold, max_frame_lost):
+# 		super(Tracker, self).__init__()
+# 		self.dist_threshold = dist_threshold
+# 		self.max_frame_lost = max_frame_lost
+# 		self.trackId = 0
+# 		self.tracks = []
+# 		# self.cur_frame = []
+# 		# self.last_frame = []
+
+
+# 	def update(self, detections,contours):
+# 		# self.cur_frame = []
+# 		if len(self.tracks) == 0:
+# 			for i in range(detections.shape[0]):
+# 				track = Tracks(detections[i],self.trackId)
+# 				# track.predict(detections[i],contours[i])
+# 				self.trackId +=1
+# 				self.tracks.append(track)
+# 				# self.cur_frame.append(i)
+# 			# self.last_frame = self.cur_frame
+# 			# return
+
+# 		N = len(self.tracks)
+# 		M = len(detections)
+# 		cost = []
+# 		for i in range(N):
+# 			# ind = self.last_frame[i]
+# 			print(str(self.tracks[i].trackId) + str(self.tracks[i].trace))
+# 			diff = np.linalg.norm(self.tracks[i].prediction - detections.reshape(-1,2), axis=1)
+# 			cost.append(diff)
+
+# 		cost = np.array(cost)*0.1
+# 		row, col = linear_sum_assignment(cost)
+# 		assignment = [-1]*N
+# 		for i in range(len(row)):
+# 			assignment[row[i]] = col[i]
+
+# 		unassigned_tracks = []
+
+# 		for i in range(len(assignment)):
+# 			if assignment[i] != -1:
+# 				if (cost[i][assignment[i]] > self.dist_threshold):
+# 					assignment[i] = -1
+# 					unassigned_tracks.append(i)
+# 					# self.tracks[i].lost_frames += 1
+# 					#self.tracks[i].self_play
+# 				else:
+# 					# self.tracks[i].predict(detections[assignment[i]],contours[assignment[i]])
+# 					# self.cur_frame.append(i)
+# 					self.tracks[i].skipped_frames +=1
+			
+# 			# else:
+# 			# 	unassigned_tracks.append(i)
+# 			# 	self.tracks[i].lost_frames += 1
+# 			# 	self.tracks[i].self_play
+		
+
+
+
+# 		del_tracks = []
+# 		for i in range(len(self.tracks)):
+# 			if self.tracks[i].skipped_frames > self.max_frame_lost:
+# 				del_tracks.append(i)
+
+# 		if len(del_tracks) > 0:
+# 			for i in range(len(del_tracks)):
+# 				del self.tracks[i]
+# 				del assignment[i]
+
+# 		for i in range(len(detections)):
+# 			if i not in assignment:
+# 				track = Tracks(detections[i],self.trackId)
+# 				# track.predict(detections[i],contours[i])
+# 				self.trackId +=1
+# 				self.tracks.append(track)
+# 				# self.cur_frame.append(len(self.tracks)-1)
+		
+# 		# self.last_frame = self.cur_frame
+# 		for i in range(len(assignment)):
+# 			if(assignment[i] != -1):
+# 				self.tracks[i].skipped_frames = 0
+# 				self.tracks[i].predict(detections[assignment[i]], contours[assignment[i]])
+# 			self.tracks[i].trace.append(self.tracks[i].prediction)
+
+import numpy as np 
+from kalmanFilter import KalmanFilter
+from scipy.optimize import linear_sum_assignment
+from collections import deque
 
 
 class Tracks(object):
-	def __init__(self, trackId, max_frame_lost):
+	"""docstring for Tracks"""
+	def __init__(self, detection, trackId):
 		super(Tracks, self).__init__()
 		self.KF = KalmanFilter()
-		self.trace = []
+		self.KF.predict()
+		self.KF.correct(np.matrix(detection).reshape(2,1))
+		self.trace = deque(maxlen=20)
+		self.prediction = detection.reshape(1,2)
 		self.trackId = trackId
-		self.prediction = None
-		self.lost_frames = 0
-		self.max_frame_lost = max_frame_lost
-		self.contour = []
+		self.skipped_frames = 0
 
 	def predict(self,detection,contour):
-		pred = self.KF.predict()
-		if len(self.trace)==0:
-			self.prediction = detection.reshape(1,2)
-		else:
-			self.prediction = np.array(pred).reshape(1,2)
+		self.prediction = np.array(self.KF.predict()).reshape(1,2)
 		self.KF.correct(np.matrix(detection).reshape(2,1))
-		self.trace.append(detection)
 		self.contour = contour
-	
-	def self_play(self):
-		self.trace.append(self.KF.predict())
 
 
 class Tracker(object):
-	def __init__(self, dist_threshold, max_frame_lost):
+	"""docstring for Tracker"""
+	def __init__(self, dist_threshold, max_frame_skipped, max_trace_length):
 		super(Tracker, self).__init__()
 		self.dist_threshold = dist_threshold
-		self.max_frame_lost = max_frame_lost
+		self.max_frame_skipped = max_frame_skipped
+		self.max_trace_length = max_trace_length
 		self.trackId = 0
 		self.tracks = []
-		self.cur_frame = []
-		self.last_frame = []
-
 
 	def update(self, detections,contours):
-		self.cur_frame = []
 		if len(self.tracks) == 0:
 			for i in range(detections.shape[0]):
-				track = Tracks(self.trackId,self.max_frame_lost)
-				track.predict(detections[i],contours[i])
+				track = Tracks(detections[i], self.trackId)
 				self.trackId +=1
 				self.tracks.append(track)
-				self.cur_frame.append(i)
-			self.last_frame = self.cur_frame
-			return
 
-		N = len(self.last_frame)
+		N = len(self.tracks)
 		M = len(detections)
 		cost = []
 		for i in range(N):
-			ind = self.last_frame[i]
-			diff = np.linalg.norm(self.tracks[ind].prediction - detections.reshape(-1,2), axis=1)
+			diff = np.linalg.norm(self.tracks[i].prediction - detections.reshape(-1,2), axis=1)
 			cost.append(diff)
 
 		cost = np.array(cost)*0.1
@@ -65,30 +169,19 @@ class Tracker(object):
 		for i in range(len(row)):
 			assignment[row[i]] = col[i]
 
-		unassigned_tracks = []
+		un_assigned_tracks = []
 
 		for i in range(len(assignment)):
 			if assignment[i] != -1:
 				if (cost[i][assignment[i]] > self.dist_threshold):
 					assignment[i] = -1
-					unassigned_tracks.append(i)
-					self.tracks[i].lost_frames += 1
-					#self.tracks[i].self_play
+					un_assigned_tracks.append(i)
 				else:
-					self.tracks[i].predict(detections[assignment[i]],contours[assignment[i]])
-					self.cur_frame.append(i)
-			
-			else:
-				unassigned_tracks.append(i)
-				self.tracks[i].lost_frames += 1
-				self.tracks[i].self_play
-		
-
-
+					self.tracks[i].skipped_frames +=1
 
 		del_tracks = []
 		for i in range(len(self.tracks)):
-			if self.tracks[i].lost_frames > self.max_frame_lost:
+			if self.tracks[i].skipped_frames > self.max_frame_skipped :
 				del_tracks.append(i)
 
 		if len(del_tracks) > 0:
@@ -98,25 +191,13 @@ class Tracker(object):
 
 		for i in range(len(detections)):
 			if i not in assignment:
-				track = Tracks(self.trackId,self.max_frame_lost)
-				track.predict(detections[i],contours[i])
+				track = Tracks(detections[i], self.trackId)
 				self.trackId +=1
 				self.tracks.append(track)
-				self.cur_frame.append(len(self.tracks)-1)
-		
-		self.last_frame = self.cur_frame
 
 
-
-
-
-
-
-
-
-
-
-		
-
-
-
+		for i in range(len(assignment)):
+			if(assignment[i] != -1):
+				self.tracks[i].skipped_frames = 0
+				self.tracks[i].predict(detections[assignment[i]], contours[assignment[i]])
+			self.tracks[i].trace.append(self.tracks[i].prediction)
